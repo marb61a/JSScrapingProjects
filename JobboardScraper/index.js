e24e57c2c77ec6032ec8feb4bb5436df80941238
Martin <marb61a@gmail.com>
2020-09-30 19:24:31 +0100

    Updated index.js to fetch jobs from github

--- JobboardScraper/index.js
+++ JobboardScraper/index.js
@@ -1,8 +1,27 @@
 const Sheet  = require('./sheet');
+const fetch = require('node-fetch');
 
 (async function(){
+    // Using github jobs
+    const res = await fetch('https://jobs.github.com/positions?location=remote');
+
+    // Works on the response object returned by fetch
+    const json = await res.json();
+
+    const rows = json.map(job => {
+        return {
+            company: job.company,
+            title: job.title,
+            location: job.location,
+            date: job.created_at,
+            url: job.url
+        }
+
+    });
+
     const sheet = new Sheet();
     await sheet.load();
 
-    
+    console.log({json});
+
 })();
