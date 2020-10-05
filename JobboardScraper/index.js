4cf635fef954aae56351d4a410278d73381a39be
Martin <marb61a@gmail.com>
2020-10-05 19:58:41 +0100

    Updated index.js and complete scraper

--- JobboardScraper/index.js
+++ JobboardScraper/index.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 
 async function scrapePage(i){
     // Using github jobs
-    const res = await fetch('https://jobs.github.com/positions?location=remote');
+    const res = await fetch(`https://jobs.github.com/positions.json?page=${i}&search=code`);
 
     // Works on the response object returned by fetch
     const jsonFetch = await res.json();
@@ -22,13 +22,12 @@ async function scrapePage(i){
 }
 
 (async function(){
-    // const sheet = new Sheet();
-    // await sheet.load();
     let i = 1;
     let rows = [];
 
     while(true){
-        const newRows = await pageXOffset.scrapePage(i);
+        const newRows = await scrapePage(i);
+        console.log("New rows length", newRows.length);
 
         if(newRows.length === 0) break;
 
@@ -36,5 +35,10 @@ async function scrapePage(i){
         i++;
     }
 
-    console.log(total);
+    // const sheet = new Sheet();
+    // await sheet.load();
+    // await sheet.addRows(rows);
+    console.log('total rows length : ', rows);
+
+    // console.log(total);
 })();
